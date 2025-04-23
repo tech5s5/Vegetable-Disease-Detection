58c5c5f1964ccffdd5019bad04da52e2420ff6ad
tech5s5 <tech2345s55@gmail.com>
2025-04-23 14:06:37 +0530

    Add Initial File

--- api/main.py
+++ api/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 
 
 
-MODEL = tf.keras.models.load_model('../saved_models/1')
+MODEL = tf.keras.models.load_model('saved_models\\1')
 CLASS_NAMES = ['Potato Early Blight','Potato Late Blight','Healthy Potato','Tomato Early Blight','Tomato Late Blight','Healthy Tomato']
 
 @app.get("/ping")
